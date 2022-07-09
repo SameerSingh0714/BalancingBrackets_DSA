9a39406b173bafaee30757b12e19facd34d7d17e
sameer <sameer.singh0714@gmail.com>
2022-07-10 04:34:14 +0530

    Final commit

---
+++ BalancingBrackets/src/com/dsa/balancingbrackets/BalancingBrackets.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.dsa.balancingbrackets;
+
+import java.util.Stack;
+
+/**
+ * @author Sameer
+ *
+ */
+public class BalancingBrackets {
+	public static void main(String[] args) {
+
+		String bracketExpression = "([[{}]])";
+
+		if (checkingBalanceBracket(bracketExpression)) {
+			System.out.println("Balance Brackets");
+		} else {
+			System.out.println("Not Balance Brackets");
+		}
+
+	}
+
+	static boolean checkingBalanceBracket(String bracketExpression) {
+
+		Stack<Character> stack = new Stack<Character>();
+
+		for (int i = 0; i < bracketExpression.length(); i++) {
+			char character = bracketExpression.charAt(i);
+
+			if (character == '(' || character == '[' || character == '{') {
+				stack.push(character);
+				continue;
+			}
+			if (stack.isEmpty()) {
+				return false;
+
+			}
+
+			char c;
+
+			switch (character) {
+			case '}':
+				c = stack.pop();
+				if (c == '(' || c == '[') {
+					return false;
+				}
+
+				break;
+
+			case ')':
+				c = stack.pop();
+				if (c == '{' || c == '[') {
+					return false;
+				}
+
+				break;
+
+			case ']':
+				c = stack.pop();
+				if (c == '(' || c == '{') {
+					return false;
+				}
+
+				break;
+
+			default:
+				break;
+			}
+
+		}
+
+		return stack.isEmpty();
+
+	}
+}
